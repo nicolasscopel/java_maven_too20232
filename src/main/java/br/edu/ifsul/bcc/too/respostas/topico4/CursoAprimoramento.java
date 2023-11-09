de8c3ef142f2568b9669f69d96390857b8834e2f
nicolasscopel <nicolaspscopel@gmail.com>
2023-11-09 15:57:48 -0300

    add

---
+++ src/main/java/br/edu/ifsul/bcc/too/respostas/topico4/CursoAprimoramento.java
@@ -0,0 +1,16 @@
+
+package br.edu.ifsul.bcc.too.respostas.topico4;
+
+/**
+ *
+ * @author telmo
+ */
+public class CursoAprimoramento {
+
+    //codigo, nome, descricao, carga horaria.
+    
+    public CursoAprimoramento() {
+    }
+    
+    
+}
---
+++ src/main/java/br/edu/ifsul/bcc/too/respostas/topico4/Pedido.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.bcc.too.respostas.topico4;
+
+/**
+ *
+ * @author telmo
+ */
+public class Pedido {
+    
+    // atributos: codigo, data, valor_total, cliente.
+
+    public Pedido() {
+    }
+    
+    
+    
+}
